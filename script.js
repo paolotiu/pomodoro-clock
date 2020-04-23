ce820d6bf9a93fbdca74cbe91d2db65f2d671277
Gabriel Paolo Tiu <paolotiu17@gmail.com>
2020-04-23 16:30:42 +0800

    js logic finished

--- script.js
+++ script.js
@@ -2,10 +2,14 @@
 
 let stopTimer = false;
 let interval;
-let SessionTime = 25;
+let sessionTime = 25;
+let timeleft = 25*60;
 let BreakTime = 5;
 let SessionAmount = 4;
-const display = document.getElementById('time');
+let display = document.getElementById('time');
+
+let isSession = true;
+let isBreak = false;
 
 // buttons and stuff
 const UpSession = document.querySelector('#up-session');
@@ -18,144 +22,188 @@ const sessionOrBreak = document.querySelector('.session');
 const SessionAmountUp = document.querySelector('#repeat-up');
 const SessionAmountdown = document.querySelector('#repeat-down');
 const SessionRepeatTimes = document.querySelector('.repeat-times');
-const StopButton = document.querySelector('.stop');
+
 const ResetButton = document.querySelector('.reset');
 const PlayButton = document.querySelector('.play');    
-const PauseButton = document.querySelector('.pause');
+
+const sessionShow = document.querySelector('#sessions-container')
+const SessionsLeft = document.querySelector('#sessions-left')
+sessionShow.style.display = 'none'; //hides the div
+
+//i think we can put it here
 
 
 
+let running = 0;
+function startPause(){ 
+  //we need show sessions left only when we started
+  sessionShow.style.display = 'block'; //shows div
+  
+    if (running === 0){ //checks if we started
+        running = 1;
+        increment();
+        PlayButton.innerHTML = "Pause";
+    } else { // checks if we paused
+        running = 0
+        PlayButton.innerHTML = "Resume"
+    }
+}
+function reset(){ 
+    sessionShow.style.display = 'none';
+    running = 0;
+    timeleft = (sessionTime*60)+1; 
+    PlayButton.innerHTML = "Start"
+    SessionAmount = parseInt(SessionRepeatTimes.innerText);
+    sessionTime = 25;
+    timeleft = 25*60;
+    BreakTime = 5;
+    display.innerHTML = sessionTime + ':00';
+    displayBreakTime.innerText = BreakTime;
+    displaySessionTime.innerText = sessionTime;
+    
+    // and hide it when we reseted
+}
 
-function startTimer(seconds, oncomplete) {
-    var startTime, timer, obj, ms = seconds*1000;
+function increment(){ // time function
+    SessionsLeft.innerText = SessionAmount;
+    
 
-    //displays if its a session or break
-    // if (seconds/60 == SessionTime){
-    //     sessionOrBreak.innerText = 'Session'
-    // }
-    // else{
-    //     sessionOrBreak.innerText = 'Break'
-    // }
-
-    obj = {};
-    obj.resume = function() {
-        startTime = new Date().getTime();
-        timer = setInterval(obj.step,250); // adjust this number to affect granularity
-                            // lower numbers are more accurate, but more CPU-expensive
-    };
-    obj.pause = function() {
-        console.log('hi')
-        ms = obj.step();
-        clearInterval(timer);
-    };
-    obj.step = function() {
-        var now = Math.max(0,ms-(new Date().getTime()-startTime)),
-            m = Math.floor(now/60000), s = Math.floor(now/1000)%60;
-        s = (s < 10 ? "0" : "")+s;
-        display.innerText = m+":"+s;
-        if( now == 0) {
-            clearInterval(timer);
-            obj.resume = function() {};
-            if( oncomplete){
-                oncomplete()
-            } ;
-        }
-        return now;
-    };
-    obj.stop = function() {
-        obj.pause();
-        display.innerText = SessionTime + ':' + '00';
-        ms = seconds*1000;
+    if(SessionAmount == 0){
+        sessionOrBreak.innerText = 'Done!'
+        return; 
     }
 
-    obj.resume();
-    return obj;
+    if(isSession === true){
+        sessionOrBreak.innerText = 'Session'
+    }
+    else{
+        sessionOrBreak.innerText = 'Break'
+    }
+
+    if(running == 1){
+        setTimeout(function(){
+            
+            timeleft--;
+            var mins = Math.floor(timeleft/60);
+            let secs = Math.floor(timeleft%60);
+            if(mins < 10) {
+                mins = '0' + mins;
+            }
+            if (secs <10) {
+                secs = '0' + secs
+            }
+            display.innerHTML = mins + ':' + secs;
+
+            if(timeleft == 0) {
+                if(SessionAmount > 0){ // so it wont go negative...and this skips the last break i think
+                    switchTimer(); //so here we can make switcher function to replace time session/break
+                }
+                else{
+                    return;
+                }
+                
+                
+            }
+            increment();
+        }, 1000);
+    }
+    
+}
+// now we need counter that counts sessionslol let me try lol kk
+//fuuuuuuu last time lol ok your turn
+function switchTimer() {
+    if (SessionAmount > 0 ){ 
+        if (isSession === true) { // if session end
+            isSession = false;
+            isBreak = true;
+            timeleft = sessionTime*60;
+            SessionAmount--;
+
+            console.log(SessionAmount)
+        } else if (isBreak === true ) { //if break end
+            isBreak = false;
+            isSession= true
+            timeleft = BreakTime*60;
+        }
+    } else {
+        running = 0; // it should work 
+        return; 
+        
+    }
+        
+    //if session - do this
+    // if break - do this
 }
 
 
-let isStarted = false;
 
 let timer;
 
-PlayButton.onclick = () => {
-    
-    if(isStarted === false ){ 
-        timer = startTimer(SessionTime*10,() => startTimer(BreakTime*10)); 
-        //how we can start timer again? uhh it should be ^^
-        console.log('1')
-        startTimer(SessionTime*10,() => startTimer(BreakTime*10));
-        console.log('2')
-        
-        
-        
-        
-        //just to check
-        
-        isStarted = true;
-        //let me try kk
-    }
-    else{
-        timer.resume();
-    }
+PlayButton.addEventListener('click', startPause);
 
-};
-
-function repeatTimes (){
-    for(let i = 0; i < SessionAmount; i++){
-        if (i%2 == 0){
-            timer = startTimer(SessionTime*10,() => startTimer(BreakTime*10));
-        }
-    }
-}
 
 ResetButton.onclick = () => {
-    if(isStarted === true){
-        timer.stop();
-    }
-    
-    SessionTime = 25;
-    BreakTime = 5;
-    displaySessionTime.innerText = SessionTime;
-    display.innerText = SessionTime + ':' + '00'
-    displayBreakTime.innerText = BreakTime;
+    reset();
 }
-PauseButton.onclick = () => timer.pause();
-StopButton.onclick = () => timer.stop();
 
 SessionAmountUp.onclick = function(){
+    if(running == 1){
+        return;
+    }
     SessionAmount++;
     SessionRepeatTimes.innerText = SessionAmount;
 }
 
 SessionAmountdown.onclick = function(){
+    if(running == 1){
+        return;
+    }
+
+    if(SessionAmount == 1){
+        return;
+    }
     SessionAmount--;
     SessionRepeatTimes.innerText = SessionAmount;
 }
 
 UpSession.onclick = function(){
-    SessionTime++;
-    isStarted = false;
-    displaySessionTime.innerText = SessionTime;
-    display.innerText = SessionTime + ':' + '00'
+    if(running == 1){
+        return;
+    }
+    sessionTime++;
+    timeleft = sessionTime*60;
+    displaySessionTime.innerText = sessionTime;
+    display.innerText = sessionTime + ':' + '00'
 }
 
 DownSession.onclick = function(){
-    //do not let session time go below 1
-    if(SessionTime == 1){
+    if(running == 1){
         return;
     }
-    isStarted = false;
-    SessionTime--;
-    displaySessionTime.innerText = SessionTime;
-    display.innerText = SessionTime + ':' + '00'
+    //do not let session time go below 1
+    if(sessionTime == 1){
+        return;
+    }
+    
+    sessionTime--;
+    timeleft = sessionTime*60;
+    displaySessionTime.innerText = sessionTime;
+    display.innerText = sessionTime + ':' + '00'
+
 }
 
 UpBreak.onclick = function(){
+    if(running == 1){
+        return;
+    }
     BreakTime++;
     displayBreakTime.innerText = BreakTime;
 }
 
 DownBreak.onclick = function(){
+    if(running == 1){
+        return;
+    }
     //do not let session time go below 1
     if(BreakTime == 1){
         return;
@@ -163,6 +211,5 @@ DownBreak.onclick = function(){
     BreakTime--;
     displayBreakTime.innerText = BreakTime;
 }
-
 
 
